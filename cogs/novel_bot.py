a50e0ee945b6205b8ad07d33ec2c11a6018e096a
ymmxl <limmyyingran@gmail.com>
2020-04-15 05:09:42 +0800

    fixed getting rate

--- cogs/novel_bot.py
+++ cogs/novel_bot.py
@@ -1,6 +1,6 @@
 import json,os,discord,requests,pytz,sys
 from datetime import datetime as dt
-from discord.ext import commands
+from discord.ext import commands, tasks
 from decimal import *
 import config
 class novel_bot(commands.Cog):
@@ -8,11 +8,19 @@ class novel_bot(commands.Cog):
 		self.client = client
 		self.api_url = "https://novelship.com/api/products/PRODUCT_ID/offer-lists?where[active:eq]=true&page[number]=0&page[size]=1000"
 		self.search_url = "https://novelship.com/api/products/search?where[search]=KEYWORD&where[active:eq]=true&page[number]=0&page[size]=1"
-
+		self.rate = 1
 	@commands.Cog.listener()
 	async def on_ready(self):
+		#TASKS LOOP CANNOT BE PROMPTED AFTER !RELOAD COMMAND#
+		self.get_rates.start()
 		print("{} Novelship module logged in!".format(self.client.user.name))
-	
+	@tasks.loop(hours=6)
+	async def get_rates(self):
+		r = requests.get("https://free.currconv.com/api/v7/convert?q=SGD_MYR&compact=ultra&apiKey=78f7ab84e1762ef7aec7").json()
+		self.rate = r["SGD_MYR"]
+	@commands.command()
+	async def r(self,message):
+		await message.channel.send("{}".format(str(self.rate)))
 	@commands.command(pass_context=True)
 	async def novel(self,message,*,kw:str):
 		r = requests.get(self.search_url.replace("KEYWORD",kw),timeout=10).json()
@@ -36,6 +44,7 @@ class novel_bot(commands.Cog):
 		res = requests.get(self.api_url.replace("PRODUCT_ID",str(product_id)),timeout=10).json()
 		if res["total"] == 0:
 			print("No bids.")
+		"""
 		try:
 			for i in res["results"]:
 				if i["local_currency_id"] == 3:
@@ -43,10 +52,10 @@ class novel_bot(commands.Cog):
 					break
 		except KeyError:
 			pass		
-			
+		"""
 		for i in res["results"]:
-			if i["local_currency_id"] == 3:
-				i["local_price"] = round(i["local_price"]/rate)
+			if i["local_currency_id"] == 1:
+				i["local_price"] = round(i["local_price"]*self.rate)
 		if highest_bid_id:
 			highest_bid = [i for i in res["results"] if i["id"] == highest_bid_id]
 			highest_bid_price = highest_bid[0]["local_price"]
@@ -88,7 +97,7 @@ class novel_bot(commands.Cog):
 		for i in available_sizes:
 			for j,k in i.items():
 				embed.add_field(name=j,value=k,inline=True)
-		embed.set_footer(text="ymmxl Novelship Bot v1.0 [{}]".format(d))
+		embed.set_footer(text="ymmxl Novelship Bot v1.1 [{}]".format(d))
 		await message.channel.send(embed=embed)
 def setup(client):
 	client.add_cog(novel_bot(client))
