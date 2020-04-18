3ca1fda720cdfdae5981ed90acf5afc86cd1605b
ymmxl <limmyyingran@gmail.com>
2020-04-19 00:34:22 +0800

    cb typo

--- cogs/novel_bot.py
+++ cogs/novel_bot.py
@@ -16,7 +16,7 @@ class novel_bot(commands.Cog):
 		print("{} Novelship module logged in!".format(self.client.user.name))
 	@tasks.loop(hours=12)
 	async def get_rates(self):
-		h = :{
+		h = {
 		"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
 		"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 		"accept-encoding":"gzip, deflate, br",
